e741bfc84134c7aceafd189ae2b2f8de50e122d6
mAntoineSt <antoine.professionnal@gmail.com>
2020-10-14 19:08:38 -0400

    Visuel et liens de la gestion des emprunteurs et des prêteurs

--- Administration/Controles utilisateurs/Gestion_Preteurs.Designer.vb
+++ Administration/Controles utilisateurs/Gestion_Preteurs.Designer.vb
@@ -22,29 +22,106 @@ Partial Class Gestion_Preteurs
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.bDroitsAdmins = New System.Windows.Forms.Button()
+        Me.bSuppressionAdmin = New System.Windows.Forms.Button()
+        Me.bModifAdmin = New System.Windows.Forms.Button()
+        Me.bAjoutAdmin = New System.Windows.Forms.Button()
+        Me.pTitre = New System.Windows.Forms.Panel()
+        Me.lTitre = New System.Windows.Forms.Label()
+        Me.dgvListeAdministrateurs = New System.Windows.Forms.DataGridView()
+        Me.pTitre.SuspendLayout()
+        CType(Me.dgvListeAdministrateurs, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Label1
+        'bDroitsAdmins
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(412, 230)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Prêteurs"
+        Me.bDroitsAdmins.Location = New System.Drawing.Point(795, 367)
+        Me.bDroitsAdmins.Name = "bDroitsAdmins"
+        Me.bDroitsAdmins.Size = New System.Drawing.Size(142, 56)
+        Me.bDroitsAdmins.TabIndex = 12
+        Me.bDroitsAdmins.Text = "Enlever/Remettre les droits d'un administrateur"
+        Me.bDroitsAdmins.UseVisualStyleBackColor = True
+        '
+        'bSuppressionAdmin
+        '
+        Me.bSuppressionAdmin.Location = New System.Drawing.Point(795, 293)
+        Me.bSuppressionAdmin.Name = "bSuppressionAdmin"
+        Me.bSuppressionAdmin.Size = New System.Drawing.Size(142, 56)
+        Me.bSuppressionAdmin.TabIndex = 11
+        Me.bSuppressionAdmin.Text = "Supprimer un administrateur"
+        Me.bSuppressionAdmin.UseVisualStyleBackColor = True
+        '
+        'bModifAdmin
+        '
+        Me.bModifAdmin.Location = New System.Drawing.Point(795, 221)
+        Me.bModifAdmin.Name = "bModifAdmin"
+        Me.bModifAdmin.Size = New System.Drawing.Size(142, 56)
+        Me.bModifAdmin.TabIndex = 10
+        Me.bModifAdmin.Text = "Modifier un administrateur"
+        Me.bModifAdmin.UseVisualStyleBackColor = True
+        '
+        'bAjoutAdmin
+        '
+        Me.bAjoutAdmin.Location = New System.Drawing.Point(795, 148)
+        Me.bAjoutAdmin.Name = "bAjoutAdmin"
+        Me.bAjoutAdmin.Size = New System.Drawing.Size(142, 56)
+        Me.bAjoutAdmin.TabIndex = 9
+        Me.bAjoutAdmin.Text = "Ajouter un nouvel administrateur"
+        Me.bAjoutAdmin.UseVisualStyleBackColor = True
+        '
+        'pTitre
+        '
+        Me.pTitre.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.pTitre.Controls.Add(Me.lTitre)
+        Me.pTitre.Location = New System.Drawing.Point(163, 26)
+        Me.pTitre.Name = "pTitre"
+        Me.pTitre.Size = New System.Drawing.Size(436, 80)
+        Me.pTitre.TabIndex = 8
+        '
+        'lTitre
+        '
+        Me.lTitre.AutoSize = True
+        Me.lTitre.BackColor = System.Drawing.Color.Transparent
+        Me.lTitre.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.lTitre.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.lTitre.Location = New System.Drawing.Point(85, 22)
+        Me.lTitre.Name = "lTitre"
+        Me.lTitre.Size = New System.Drawing.Size(266, 31)
+        Me.lTitre.TabIndex = 0
+        Me.lTitre.Text = "Gestion des prêteurs"
+        '
+        'dgvListeAdministrateurs
+        '
+        Me.dgvListeAdministrateurs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvListeAdministrateurs.Location = New System.Drawing.Point(75, 124)
+        Me.dgvListeAdministrateurs.Name = "dgvListeAdministrateurs"
+        Me.dgvListeAdministrateurs.Size = New System.Drawing.Size(601, 323)
+        Me.dgvListeAdministrateurs.TabIndex = 7
         '
         'Gestion_Preteurs
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.bDroitsAdmins)
+        Me.Controls.Add(Me.bSuppressionAdmin)
+        Me.Controls.Add(Me.bModifAdmin)
+        Me.Controls.Add(Me.bAjoutAdmin)
+        Me.Controls.Add(Me.pTitre)
+        Me.Controls.Add(Me.dgvListeAdministrateurs)
         Me.Name = "Gestion_Preteurs"
         Me.Size = New System.Drawing.Size(1013, 473)
+        Me.pTitre.ResumeLayout(False)
+        Me.pTitre.PerformLayout()
+        CType(Me.dgvListeAdministrateurs, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents bDroitsAdmins As Button
+    Friend WithEvents bSuppressionAdmin As Button
+    Friend WithEvents bModifAdmin As Button
+    Friend WithEvents bAjoutAdmin As Button
+    Friend WithEvents pTitre As Panel
+    Friend WithEvents lTitre As Label
+    Friend WithEvents dgvListeAdministrateurs As DataGridView
 End Class
