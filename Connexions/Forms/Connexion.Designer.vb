c3b6cfd5621cb2cd039c2fda8eb1a38786f537af
mAntoineSt <antoine.professionnal@gmail.com>
2020-10-14 17:38:40 -0400

    Visuel Form de Connexion

--- Connexions/Forms/Connexion.Designer.vb
+++ Connexions/Forms/Connexion.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Connexion
     Inherits System.Windows.Forms.Form
 
     'Form remplace la méthode Dispose pour nettoyer la liste des composants.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,33 +20,83 @@ Partial Class Connexion
     'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
     'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
     'Ne la modifiez pas à l'aide de l'éditeur de code.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.LTest = New System.Windows.Forms.Label()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Connexion))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.bConfirmer = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'LTest
+        'TextBox1
         '
-        Me.LTest.AutoSize = True
-        Me.LTest.Location = New System.Drawing.Point(128, 134)
-        Me.LTest.Name = "LTest"
-        Me.LTest.Size = New System.Drawing.Size(573, 13)
-        Me.LTest.TabIndex = 0
-        Me.LTest.Text = "TEST, SI VOUS VOYEZ ÇA. CRÉEZ UNE AUTRE LABEL AVEC UN AUTRE MESSAGE ET PUSH LE TO" &
-    "UT - Antoine"
+        Me.TextBox1.Location = New System.Drawing.Point(158, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(223, 20)
+        Me.TextBox1.TabIndex = 0
         '
-        'Form1
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(158, 90)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(223, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(99, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Matricule :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Mot de passe :"
+        '
+        'bConfirmer
+        '
+        Me.bConfirmer.Location = New System.Drawing.Point(169, 147)
+        Me.bConfirmer.Name = "bConfirmer"
+        Me.bConfirmer.Size = New System.Drawing.Size(121, 38)
+        Me.bConfirmer.TabIndex = 4
+        Me.bConfirmer.Text = "Confirmer"
+        Me.bConfirmer.UseVisualStyleBackColor = True
+        '
+        'Connexion
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.LTest)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(458, 208)
+        Me.Controls.Add(Me.bConfirmer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Connexion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Connexion"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents LTest As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents bConfirmer As Button
 End Class
--- Connexions/Forms/Connexion.vb
+++ Connexions/Forms/Connexion.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Connexion
 
+    'Sert à vérifier si le matricule et le mot de passe est correct pour se connecter
+    Private Sub bConfirmer_Click(sender As Object, e As EventArgs) Handles bConfirmer.Click
+        'Recherche dans la BD pour voir si le prêteur ou l'administrateur est là.
+
+    End Sub
 End Class
