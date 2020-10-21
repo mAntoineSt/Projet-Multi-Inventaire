0e2a4edde06fbcb4f49cd36e8bd66445d8c878c7
Marc-Antoine Quessy <papibenjie@gmail.com>
2020-10-21 16:50:38 -0400

    Revert "."
    This reverts commit e7da66603a0f60fa75d106fb98d64ecd350aee80.

--- Entity/EntityTest.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class EntityTest
-    Private Sub EntityTest_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim users = New testEntities().users
-
-        Dim majeur = users.Where(Function(usr) usr.FirstName.Equals("Pierre")).Any(Function(usr) usr.BirthDate.Year.Equals(1997))
-
-
-    End Sub
-End Class
