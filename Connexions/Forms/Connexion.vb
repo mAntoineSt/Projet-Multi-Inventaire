589da8c41a2ccb22a84e4cb192792ec192335e87
mAntoineSt <antoine.professionnal@gmail.com>
2020-10-30 10:48:57 -0400

    présentation

--- Connexions/Forms/Connexion.vb
+++ Connexions/Forms/Connexion.vb
@@ -1,4 +1,13 @@
-﻿Public Class Connexion
+﻿'Importation nécessaire
+Imports MySql.Data.MySqlClient
+
+Public Class Connexion
+    'Variables nécessaires
+    Dim con As MySqlConnection = New MySqlConnection("Server=localhost;Database=projet_multi;Uid=root;Pwd='';Port=3308")
+    Dim reader As MySqlDataReader
+    Dim commande As New MySqlCommand
+    Dim mdp As String
+
 
     'Sert à vérifier si le matricule et le mot de passe est correct pour se connecter
     Private Sub bConfirmer_Click(sender As Object, e As EventArgs) Handles bConfirmer.Click
@@ -10,8 +19,46 @@
             Return
         End If
 
+        If tbMatricule.Text.Length <> 7 Then
+            MessageBox.Show("Le matricule n'est pas au bon format (7 chiffres)", "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
         'Recherche dans la BD pour voir si le prêteur ou l'administrateur est là.
-        '...
+        mdp = ""
+        Try
+            'Connexion et commande
+            commande.Connection = con
+            commande.CommandText = "select motdepasse from individus where id_individu='test';"
+            con.Open()
+            reader = commande.ExecuteReader
+            While (reader.Read)
+                mdp = reader(0)
+            End While
+
+            'Vérifie si la base de donnée a retourné quelque chose, si ce n'est pas le cas : ERREUR.
+            If String.IsNullOrWhiteSpace(mdp) Then
+                MessageBox.Show("Le matricule que vous avez entré n'existe pas.", "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                con.Close()
+                Return
+            End If
+
+            'Vérifie si le mot de passe trouvé dans la base de donné est le bon.
+            If mdp <> tbMdp.Text Then
+                MessageBox.Show("Le mot de passe que vous avez entré n'est pas le bon", "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                con.Close()
+                Return
+            End If
+
+            'Referme la connexion pour que cela ne cause pas de problèmes pour la prochaine demande
+            con.Close()
+
+            commande.CommandText = "select motdepasse from individus where id_individu='test';"
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
 
 
         'Si tout est correct
